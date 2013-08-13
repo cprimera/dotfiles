55d8aa57df27074a6ed6b2a6d849fb2cd9012362
Christopher Primerano <cprimera@sympatico.ca>
2013-08-13 18:36:46 -0400

    fixed bug in install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ git clone https://github.com/cprimera/dotfiles /home/`whoami`/dotfiles
 cd /home/`whoami`/dotfiles
 for i in *
 do
-	if [ "$i" != "README.md" ]
+	if [ "$i" != "README.md" ] && [ "$i" != "install.sh" ]
 	then
 		ln -s $i /home/`whoami`/.${i}
 	fi
