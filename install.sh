be28accd2ff0fcf3f178f2845b5d41d73c93a5e7
Christopher Primerano <cprimera@sympatico.ca>
2013-08-13 20:14:49 -0400

    updated install script

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
-git clone https://github.com/cprimera/dotfiles /home/`whoami`/dotfiles
-cd /home/`whoami`/dotfiles
+git clone https://github.com/cprimera/dotfiles ~/dotfiles
+cd ~/dotfiles
+git submodule init
+git submodule update
+git clone https://gist.github.com/6226629.git
 for i in *
 do
-	if [ "$i" != "README.md" ] && [ "$i" != "install.sh" ]
+	if [ "$i" != "README.md" ] && [ "$i" != "install.sh" ] && [ "$i" != "6226629" ]
 	then
-		ln -s $i /home/`whoami`/.${i}
+		ln -s ~/dotfiles/$i ~/.${i}
 	fi
 done
+ln -s ~/dotfiles/6226629/cprimera.zsh-theme ./oh-my-zsh/themes/cprimera.zsh-theme
+
