ae91ee4b94c57f43dcac803a68b8308aa915699e
Christopher Primerano <cprimera@mac.com>
2021-10-08 18:54:53 -0400

    Only symlink actual dotfiles
    Only symlinks actual dotfiles. Uses an include list instead of an
    exclude list now. Makes it easier to since there are less files to
    link than to exclude from linking.

--- install.sh
+++ install.sh
@@ -1,22 +1,19 @@
 populate () {
-	cd ~/dotfiles
-	for i in *
-	do
-		if [ "$i" != "README.md" ] && [ "$i" != "install.sh" ]
-		then
-			ln -s ~/dotfiles/$i ~/.${i}
-		fi
-	done
+  cd ~/dotfiles
+  for i in {zshrc,vimrc,screenrc}
+  do
+    ln -s ~/dotfiles/$i ~/.${i}
+  done
 }
 
 install_ohmyzsh () {
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-	curl -fsSL https://gist.githubusercontent.com/cprimera/6226629/raw/0e9cd73e48d2a967ead3514aaaba187242eff48b/cprimera.zsh-theme > ~/.oh-my-zsh/themes/cprimera.zsh-theme
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  curl -fsSL https://gist.githubusercontent.com/cprimera/6226629/raw/0e9cd73e48d2a967ead3514aaaba187242eff48b/cprimera.zsh-theme > ~/.oh-my-zsh/themes/cprimera.zsh-theme
 }
 
 if [ "$1" != "--local" ] && [ "$1" != "-l" ]
 then
-	git clone https://github.com/cprimera/dotfiles ~/dotfiles
+  git clone https://github.com/cprimera/dotfiles ~/dotfiles
 fi
 
 populate
